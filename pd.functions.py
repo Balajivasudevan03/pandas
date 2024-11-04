3aeafec4a8e8d30dfb9862b9b375eeb38205e7f5
Balajivasudevan03 <balajivasudevan.03.11.96@gamil.com>
2024-11-04 20:18:46 +0530

    hi

---
+++ pd.functions.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+#df = pd.read_csv("C:\Users\haris\Downloads\sample_finance_data (1).xlsx")
+
+#print(df)
+
+#import pandas as pd
+df = pd.DataFrame({"users":[1,2,3,4],"age":[20,21,22,23]})
+print(df)
+df1 = pd.read_excel("C:/Users/haris/Downloads/sample_finance_data.xlsx")
+print(df1)
+import numpy as np
+print(np.array([1,2,3,4]))
+
+df2 = pd.read_excel('C:/Users/haris/Downloads/dosages.csv')
+print(df2)
+
